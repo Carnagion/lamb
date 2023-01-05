997e9ed4beb4f21bf27d9767407558ba0e9bfdf5
Carnagion <carnagion.games@gmail.com>
2023-01-05 09:26:56 +0100

    Add lexer

---
+++ src/lexer.rs
@@ -0,0 +1,31 @@
+use logos;
+use logos::Lexer;
+use logos::Logos;
+
+#[derive(Clone, Debug, Eq, Hash, Logos, PartialEq)]
+pub enum Token<'s> {
+    #[token("λ")]
+    Lambda,
+    #[regex(r#"[^λ\.()\s=#]+"#)]
+    Ident(&'s str),
+    #[token(".")]
+    Dot,
+    #[token("(")]
+    OpenParens,
+    #[token(")")]
+    CloseParens,
+    #[token("=")]
+    Equals,
+    #[regex(r#"\s+"#)]
+    Whitespace,
+    #[regex(r#"#.*"#, Token::line_comment)]
+    LineComment(&'s str),
+    #[error]
+    Unknown,
+}
+
+impl Token<'_> {
+    fn line_comment<'s>(lexer: &Lexer<'s, Token<'s>>) -> &'s str {
+        &lexer.slice()[1..]
+    }
+}
