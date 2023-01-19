172062452d954dffee471ca69a58d2efc253852c
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-01-19 13:53:25 +0000

    Remove unused enum

--- src/repl/command.rs
+++ src/repl/command.rs
@@ -1,13 +1,5 @@
-use crate::ReducedTerm;
 use crate::Term;
 use crate::repl::Statement;
-
-pub enum CommandOutcome<T> {
-    StatementExecuted,
-    BindingOverwritten(T),
-    TermReduced(ReducedTerm<T>),
-    ReductionLimitReached(usize),
-}
 
 pub enum Command<T> {
     Reduce(Term<T>),
