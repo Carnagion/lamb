3f776272fde12cae3d60e3dfb0fd531e9fa5c911
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-01-19 13:21:18 +0000

    Add better colouring to output

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ fn main() {
     let reduce_limit = 1000;
     let mut binds = HashMap::new();
 
+    let mut color_gen = ColorGenerator::new();
+
     loop {
         let mut source = String::new();
         if let Err(error) = io::stdin().read_line(&mut source) {
@@ -42,7 +44,7 @@ fn main() {
         let command = match parse_result {
             Ok(command) => command,
             Err(errors) => {
-                report_syntax_error(&source, errors);
+                report_syntax_error(&source, errors, &mut color_gen);
                 continue;
             },
         };
@@ -54,16 +56,17 @@ fn main() {
                 report_term_reduced(&source, count);
                 println!("{}", reduced.term());
                 if count >= reduce_limit {
-                    report_reduce_limit_reached(&source, reduce_limit);
+                    report_reduce_limit_reached(&source, reduce_limit, color_gen.next());
                 }
             },
             Command::Exec(statements) => for statement in statements {
                 match statement {
                     Statement::Bind(name, term) => {
+                        let color = color_gen.next();
                         let inserted = binds.insert(name.clone(), term);
-                        report_binding_added(&source, &name);
+                        report_binding_added(&source, &name, color);
                         if inserted.is_some() {
-                            report_binding_overwritten(&source, &name);
+                            report_binding_overwritten(&source, &name, color);
                         }
                     },
                 }
@@ -80,11 +83,15 @@ fn report_read_error(source: impl AsRef<str>, error: IoError) {
         .unwrap();
 }
 
-fn report_syntax_error(source: impl AsRef<str>, errors: Vec<Simple<Token>>) {
+fn report_syntax_error(source: impl AsRef<str>, errors: Vec<Simple<Token>>, color_gen: &mut ColorGenerator) {
     errors.into_iter()
         .fold(Report::build(ReportKind::Error, (), 0)
-            .with_message("Invalid syntax"), |report, error| report.with_label(Label::new(into_char_span(error.span(), &source))
-                .with_message(format!("{}", error))))
+            .with_message("Invalid syntax"), |report, error| {
+                let color = color_gen.next();
+                report.with_label(Label::new(into_char_span(error.span(), &source))
+                    .with_message(format!("{}", error.fg(color)))
+                    .with_color(color))
+            })
         .finish()
         .eprint(Source::from(source))
         .unwrap();
@@ -98,28 +105,29 @@ fn report_term_reduced(source: impl AsRef<str>, count: usize) {
         .unwrap();
 }
 
-fn report_reduce_limit_reached(source: impl AsRef<str>, reduce_limit: usize) {
+fn report_reduce_limit_reached(source: impl AsRef<str>, reduce_limit: usize, color: Color) {
     Report::build(ReportKind::Warning, (), 0)
         .with_message("Reduction limit reached")
         .with_label(Label::new(0..source.as_ref().chars().count() - 1)
-            .with_message("possibly divergent term"))
-        .with_note(format!("current reduction limit is {}", reduce_limit))
+            .with_message("possibly divergent term")
+            .with_color(color))
+        .with_note(format!("current reduction limit is {}", reduce_limit.fg(color)))
         .finish()
         .print(Source::from(source))
         .unwrap();
 }
 
-fn report_binding_added(source: impl AsRef<str>, name: impl AsRef<str>) {
+fn report_binding_added(source: impl AsRef<str>, name: impl AsRef<str>, color: Color) {
     Report::<Range<usize>>::build(REPORT_KIND_INFO, (), 0)
-        .with_message(format!("Binding {} added", name.as_ref()))
+        .with_message(format!("Binding {} added", name.as_ref().fg(color)))
         .finish()
         .print(Source::from(source))
         .unwrap();
 }
 
-fn report_binding_overwritten(source: impl AsRef<str>, name: impl AsRef<str>) {
+fn report_binding_overwritten(source: impl AsRef<str>, name: impl AsRef<str>, color: Color) {
     Report::<Range<usize>>::build(ReportKind::Warning, (), 0)
-        .with_message(format!("Binding {} overwritten", name.as_ref()))
+        .with_message(format!("Binding {} overwritten", name.as_ref().fg(color)))
         .finish()
         .print(Source::from(source))
         .unwrap();
