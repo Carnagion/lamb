ba54d03a4121a01bd471ae933d04238a0eeed53c
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-01-19 22:29:24 +0000

    Improve parser

--- src/repl/parser.rs
+++ src/repl/parser.rs
@@ -6,22 +6,30 @@ use crate::repl::Statement;
 use crate::repl::lexer::Token;
 
 pub fn command_parser<'s>() -> impl Parser<Token<'s>, Command<String>, Error = Simple<Token<'s>>> {
-    statement_parser().repeated()
+    let exec = statement_parser().repeated()
         .at_least(1)
-        .map(Command::Exec)
-        .or(term_parser().map(Command::Reduce))
+        .map(Command::Exec);
+
+    let reduce = term_parser().map(Command::Reduce);
+
+    let exit = just(Token::Ident("exit")).ignore_then(filler_parser())
+        .to(Command::Exit);
+
+    let display = just(Token::Ident("display")).ignore_then(filler_parser())
+        .ignore_then(ident_parser())
+        .map(Command::Display);
+        
+    let debug = just(Token::Ident("debug")).ignore_then(filler_parser())
+        .ignore_then(ident_parser())
+        .map(Command::Debug);
+    
+    let limit = just(Token::Ident("limit")).ignore_then(filler_parser())
+        .ignore_then(number_parser().or_not())
+        .map(Command::Limit);
+    
+    exec.or(reduce)
         .or(just(Token::Colon).ignore_then(filler_parser())
-            .ignore_then(just(Token::Ident("exit")).ignore_then(filler_parser())
-                .to(Command::Exit)
-                .or(just(Token::Ident("display")).ignore_then(filler_parser())
-                    .ignore_then(ident_parser())
-                    .map(Command::Display))
-                .or(just(Token::Ident("debug")).ignore_then(filler_parser())
-                    .ignore_then(ident_parser())
-                    .map(Command::Debug))
-                .or(just(Token::Ident("limit")).ignore_then(filler_parser())
-                    .ignore_then(number_parser().or_not())
-                    .map(Command::Limit)))
+            .ignore_then(choice((exit, display, debug, limit)))
             .then_ignore(filler_parser()))
 }
 
@@ -34,8 +42,7 @@ pub fn statement_parser<'s>() -> impl Parser<Token<'s>, Statement<String>, Error
 
 pub fn term_parser<'s>() -> impl Parser<Token<'s>, Term<String>, Error = Simple<Token<'s>>> {
     recursive(|term| {
-        let var = ident_parser().map(Term::var)
-            .labelled("variable");
+        let var = ident_parser().map(Term::var);
 
         let abs = just(Token::Lambda).then_ignore(filler_parser())
             .ignore_then(ident_parser().repeated())
@@ -49,16 +56,11 @@ pub fn term_parser<'s>() -> impl Parser<Token<'s>, Term<String>, Error = Simple<
 
         let app = parens.clone()
             .or(var.clone())
-            .then(abs.clone()
-                .or(var.clone())
-                .or(parens.clone())
-                .repeated()
+            .then(choice((abs.clone(), var.clone(), parens.clone())).repeated()
                 .at_least(1))
             .foldl(Term::app);
 
-        abs.or(app)
-            .or(var)
-            .or(parens)
+        choice((abs, app, var, parens))
     })
 }
 
